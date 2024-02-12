9646a950190a30709c05f74e103a8d5e1c8ab226
dazzel3 <aakk9350@naver.com>
2024-02-12 17:10:39 +0900

    style: App Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styles from './App.module.css';
 import TodoList from './components/TodoList/TodoList';
 import Header from './components/Header/Header';
 
@@ -9,9 +8,9 @@ export default function App() {
   const [filter, setFilter] = useState(filters[0]);
 
   return (
-    <div className={styles.bg}>
+    <>
       <Header filters={filters} filter={filter} onChange={setFilter} />
       <TodoList filter={filter} />
-    </div>
+    </>
   );
 }
